dc57b75251b0f40487a0d4bb9ad14dc00a46a32c
pratomosidiq123 <pratomosidiq123@gmail.com>
2025-03-09 01:21:20 +0700

    root page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,17 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
 import { Button } from "@/components/ui/button";
+import { sampleBooks } from "@/constants";
 
 export default function Home() {
   return (
     <>
-      <Button>Click me</Button>
+      <BookOverview {...sampleBooks[0]}/>
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
     </>
   );
 }
