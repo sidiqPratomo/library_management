7900bc11cb6bb019844a498f75c0906ccda6b418
pratomosidiq123 <pratomosidiq123@gmail.com>
2025-06-01 19:15:48 +0700

    add validasi admin at dashboard admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,20 +1,33 @@
 import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
 import React, { ReactNode } from 'react';
-import "@/styles/admin.css";
+import '@/styles/admin.css';
 import Sidebar from '@/components/admin/Sidebar';
 import Header from '@/components/admin/Header';
+import { db } from '@/database/drizzle';
+import { users } from '@/database/schema';
+import { eq } from 'drizzle-orm';
 
 const layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
 
   if (!session?.user?.id) redirect('/sign-in');
+
+  const isAdmin = await db
+    .select({ isAdmin: users.role })
+    .from(users)
+    .where(eq(users.id, session.user.id))
+    .limit(1)
+    .then((res) => res[0]?.isAdmin === 'ADMIN');
+
+  if (!isAdmin) redirect("/");
+
   return (
     <main className='flex min-h-screen w-full flex-row'>
       <Sidebar session={session} />
 
       <div className='admin-container'>
-        <Header session={session}/>
+        <Header session={session} />
         {children}
       </div>
     </main>
